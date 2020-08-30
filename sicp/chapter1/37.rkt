bfab1d077db31709cc48fbd7ed1e534e90794f31
Ilya Campos <campos.ilya@steelkiwi.com>
2020-08-30 12:15:44 +0300

    Exercise 1.39
    Implement procedure that calculates tangent of x, based on Lambert's continued fraction.

--- sicp/chapter1/37.rkt
+++ sicp/chapter1/37.rkt
@@ -1,6 +1,8 @@
 #lang sicp
 
+(#%require racket/math)
 (#%require rackunit)
+
 
 (define (cont-frac N D k)
   (define (cont-frac-iter i a)
@@ -9,7 +11,6 @@
         (cont-frac-iter (- i 1) (/ (N i) (+ (D i) a)))))
   
   (cont-frac-iter k 0))
-
 
 (define (golden-ratio precision)
   (/ 1 (cont-frac (lambda (_) 1.0) (lambda (_) 1.0) precision)))
@@ -22,13 +23,24 @@
           1)))
 
   (+ 2 (cont-frac (lambda (_) 1.0) coeff-seq precision)))
+(define (tan-cf x precision)
+  (define (^2 num) (* num num))
+  (cont-frac (lambda (k) (if (= k 1) x (* -1 (^2 x)))) (lambda (k) (- (* 2 k) 1)) precision))
+
 
 (define-binary-check (check-in-tolerance actual expected)
   (< (abs (- actual expected)) 0.001))
 
 (define GOLDEN_RATIO 1.6180339887)
-(define EULERS_NUM 2.7182818284)
+(define EULERS_NUM (exp 1))
 (test-case
   "Should implement common continue fraction."
   (check-in-tolerance (golden-ratio 100) GOLDEN_RATIO)
   (check-in-tolerance (eulers-num 100) EULERS_NUM))
+
+(test-case
+  "Should calculate tangens of angle."
+  (check-in-tolerance (tan-cf 0 100) 0)
+  (check-in-tolerance (tan-cf pi 100) 0)
+  (check-in-tolerance (tan-cf (/ pi 4) 100) 1.0)
+  (check-in-tolerance (tan-cf 1 100) 1.55740))
