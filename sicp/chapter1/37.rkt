584d0adf904ceb03780f4114c3f5a4bd7728d6ac
Ilya Campos <campos.ilya@steelkiwi.com>
2020-11-02 10:28:52 +0200

    split file with 1.37 solution on 1.37 1.38 1.39 exercises

--- sicp/chapter1/37.rkt
+++ sicp/chapter1/37.rkt
@@ -14,33 +14,11 @@
 
 (define (golden-ratio precision)
   (/ 1 (cont-frac (lambda (_) 1.0) (lambda (_) 1.0) precision)))
-(define (eulers-num precision)
-  (define (divides-by-3? num) (= (remainder num 3) 0))
-  (define (coeff-seq k)
-    (let ([i (+ k 1)])
-      (if (divides-by-3? i)
-          (* (/ i 3) 2)
-          1)))
-
-  (+ 2 (cont-frac (lambda (_) 1.0) coeff-seq precision)))
-(define (tan-cf x precision)
-  (define (^2 num) (* num num))
-  (cont-frac (lambda (k) (if (= k 1) x (* -1 (^2 x)))) (lambda (k) (- (* 2 k) 1)) precision))
-
 
 (define-binary-check (check-in-tolerance actual expected)
   (< (abs (- actual expected)) 0.001))
 
 (define GOLDEN_RATIO 1.6180339887)
-(define EULERS_NUM (exp 1))
 (test-case
   "Should implement common continue fraction."
-  (check-in-tolerance (golden-ratio 100) GOLDEN_RATIO)
-  (check-in-tolerance (eulers-num 100) EULERS_NUM))
-
-(test-case
-  "Should calculate tangens of angle."
-  (check-in-tolerance (tan-cf 0 100) 0)
-  (check-in-tolerance (tan-cf pi 100) 0)
-  (check-in-tolerance (tan-cf (/ pi 4) 100) 1.0)
-  (check-in-tolerance (tan-cf 1 100) 1.55740))
+  (check-in-tolerance (golden-ratio 100) GOLDEN_RATIO))
