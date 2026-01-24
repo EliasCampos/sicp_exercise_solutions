076a488c67e0b0f3834a20b9595a2cb8e2d31af2
Illia Campos <camposylia@gmail.com>
2026-01-24 10:35:55 +0200

    Exercise 1.6

---
+++ sicp/chapter1/06.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (square x) (* x x))
+(define (average a b) (/ (+ a b) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+
+(new-if (= 1 1) 0 5)
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
+
+(sqrt-iter 1.0 2)
