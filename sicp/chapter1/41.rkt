2bfad61b61887330c0b5d6942c6e5799d8306c89
Illia Campos <camposylia@gmail.com>
2026-01-28 00:23:06 +0200

    Exercise 1.41 extra test

--- sicp/chapter1/41.rkt
+++ sicp/chapter1/41.rkt
@@ -11,4 +11,6 @@
   "Should implement procedure to apply function twice."
   (check-equal? ((double inc) 0) 2)
   (check-equal? ((double inc) 5) 7)
-  (check-equal? ((double (double inc)) 0) 4))
+  (check-equal? ((double (double inc)) 0) 4)
+  (check-equal? (((double double) inc) 5) 9)
+  (check-equal? (((double (double double)) inc) 5) 21))
