acca01ef405270e94f7b53dd76d7711c806e5cb0
Illia Campos <camposylia@gmail.com>
2026-01-31 23:55:33 +0200

    Exercise 2.10

---
+++ sicp/chapter2/10.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (min (car x) (cdr x)))
+(define (upper-bound x) (max (car x) (cdr x)))
+
+(define (mul-interval x y)
+  (let ([p1 (* (lower-bound x) (lower-bound y))]
+        [p2 (* (lower-bound x) (upper-bound y))]
+        [p3 (* (upper-bound x) (lower-bound y))]
+        [p4 (* (upper-bound x) (upper-bound y))])
+    (make-interval
+     (min p1 p2 p3 p4)
+     (max p1 p2 p3 p4))))
+(define (div-interval x y)
+  (if (= (upper-bound y) (lower-bound y))
+      (error "Cannot divide by an interval that spans zero")
+      (mul-interval
+       x
+       (make-interval (/ 1.0 (upper-bound y))
+                  (/ 1.0 (lower-bound y))))))
+
+(check-exn
+ #rx"[Cc]annot divide by an interval that spans zero"
+ (lambda () (div-interval (make-interval 1.0 2.0) (make-interval 3.0 3.0))))
