d032bd91dfb384ddaf9bb58ca1157bffd722815e
Illia Campos <camposylia@gmail.com>
2026-02-01 17:58:49 +0200

    Exercise 2.20 more tests

--- sicp/chapter2/20.rkt
+++ sicp/chapter2/20.rkt
@@ -14,4 +14,7 @@
 (test-case
   "Should return list of arguments with same parity as in the first number."
   (check-equal? (same-parity 1 2 3 4 5 6 7) (list 1 3 5 7))
-  (check-equal? (same-parity 2 3 4 5 6 7) (list 2 4 6)))
+  (check-equal? (same-parity 2 3 4 5 6 7) (list 2 4 6))
+  (check-equal? (same-parity 10 5 7 11 13) (list 10))
+  (check-equal? (same-parity 9 3 6 5 12) (list 9 3 5))
+  (check-equal? (same-parity 17) (list 17)))
