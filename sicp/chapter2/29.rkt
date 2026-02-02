6de93e1525740b9d07916c1b7be2ca3440f08f8f
Illia Campos <camposylia@gmail.com>
2026-02-02 22:35:43 +0200

    Exercise 2.29 c)

--- sicp/chapter2/29.rkt
+++ sicp/chapter2/29.rkt
@@ -16,19 +16,43 @@
 (define (branch-structure branch)
   (cadr branch))
 
+(define (is-binary-mobile? value)
+  (pair? value))
+
 (define (total-weight binary-mobile)
   (let ([left-struct (branch-structure (left-branch binary-mobile))]
         [right-struct (branch-structure (right-branch binary-mobile))])
-       (+ (if (pair? left-struct) (total-weight left-struct) left-struct)
-          (if (pair? right-struct) (total-weight right-struct) right-struct))))
+       (+ (struct-weight left-struct)
+          (struct-weight right-struct))))
+(define (struct-weight structure)
+  (if (is-binary-mobile? structure)
+      (total-weight structure)
+      structure))
+
+(define (is-binary-mobile-balanced? binary-mobile)
+  (let* ([left-br (left-branch binary-mobile)]
+         [right-br (right-branch binary-mobile)]
+         [left-len (branch-length left-br)]
+         [right-len (branch-length right-br)]
+         [left-struct (branch-structure left-br)]
+         [right-struct (branch-structure right-br)])
+    (and (= (* left-len (struct-weight left-struct))
+            (* right-len (struct-weight right-struct)))
+         (or (not (is-binary-mobile? left-struct))
+             (is-binary-mobile-balanced? left-struct))
+         (or (not (is-binary-mobile? right-struct))
+             (is-binary-mobile-balanced? right-struct)))))
 
 
-(define br11 (make-branch 10 6))
+(define br11 (make-branch 10 4))
 (define br12 (make-branch 5 8))
 (define bm1 (make-binary-mobile br11 br12))
-(define br21 (make-branch 15 4))
+(define br21 (make-branch 30 2))
 (define br22 (make-branch 5 bm1))
 (define bm2 (make-binary-mobile br21 br22))
+(define br31 (make-branch 13 13))
+(define br32 (make-branch 7 7))
+(define bm3 (make-binary-mobile br31 br32))
 
 (test-case
   "Left branch."
@@ -42,16 +66,22 @@
   "Branch length."
   (check-equal? (branch-length br11) 10)
   (check-equal? (branch-length br12) 5)
-  (check-equal? (branch-length br21) 15)
+  (check-equal? (branch-length br21) 30)
   (check-equal? (branch-length br22) 5))
 (test-case
   "Branch structure."
-  (check-equal? (branch-structure br11) 6)
+  (check-equal? (branch-structure br11) 4)
   (check-equal? (branch-structure br12) 8)
-  (check-equal? (branch-structure br21) 4)
+  (check-equal? (branch-structure br21) 2)
   (check-equal? (branch-structure br22) bm1))
 
 (test-case
   "Total weight."
-  (check-equal? (total-weight bm1) 14)
-  (check-equal? (total-weight bm2) 18))
+  (check-equal? (total-weight bm1) 12)
+  (check-equal? (total-weight bm2) 14))
+
+(test-case
+ "Balance check."
+ (check-true (is-binary-mobile-balanced? bm1))
+ (check-true (is-binary-mobile-balanced? bm2))
+ (check-false (is-binary-mobile-balanced? bm3)))
