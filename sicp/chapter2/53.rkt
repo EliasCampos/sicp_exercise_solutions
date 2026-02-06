3001b047e76638a6a6b6bc6bab687093c77b148d
Illia <camposylia@gmail.com>
2026-02-06 22:34:43 +0200

    Exercise 2.53 (#67)

---
+++ sicp/chapter2/53.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+(#%require rackunit)
+
+(check-equal? (list (list 'george)) '((george)))
+(check-equal? (list 'a 'b 'c) '(a b c))
+(check-equal? (cdr '((x1 x2) (y1 y2))) '((y1 y2)))
+(check-equal? (cadr '((x1 x2) (y1 y2))) '(y1 y2))
+(check-false (pair? (car '(a short list))))
+(check-false (memq 'red '((red shoes) (blue socks))))
+(check-equal? (memq 'red '(red shoes blue socks)) '(red shoes blue socks))
