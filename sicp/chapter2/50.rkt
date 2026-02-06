12c305a8c66dc805e195c5184a52cbe5132fb048
Illia Campos <camposylia@gmail.com>
2026-02-06 19:21:59 +0200

    Exercise 2.50 finish

--- sicp/chapter2/50.rkt
+++ sicp/chapter2/50.rkt
@@ -2,9 +2,25 @@
 
 (#%require sicp-pict)
 
+
 (define (flip-horizontally painter)
   (transform-painter painter
                      (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
 (paint (flip-horizontally einstein))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+(paint (rotate-180 einstein))
+
+
+(define (rotate-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+(paint (rotate-270 einstein))
