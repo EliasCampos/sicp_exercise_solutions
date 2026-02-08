8861d14417aa53f92a97cc8d491c4add21dbe4f5
Illia Campos <camposylia@gmail.com>
2026-02-08 12:05:36 +0200

    Exercise 2.61 more tests

--- sicp/chapter2/61.rkt
+++ sicp/chapter2/61.rkt
@@ -10,5 +10,8 @@
 
 (check-equal? (adjoin-set 6 '(1 3 5 7)) '(1 3 5 6 7))
 (check-equal? (adjoin-set 4 '(2 4 6 8)) '(2 4 6 8))
+(check-equal? (adjoin-set 1 '(1 2 3)) '(1 2 3))
+(check-equal? (adjoin-set 4 '(2 3 4)) '(2 3 4))
+(check-equal? (adjoin-set 5 '()) '(5))
 (check-equal? (adjoin-set 9 '(2 4 6 8)) '(2 4 6 8 9))
 (check-equal? (adjoin-set 1 '(2 4 6 8)) '(1 2 4 6 8))
