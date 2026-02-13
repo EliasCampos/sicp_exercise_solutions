dda8c759510453a6720132d93ac101850554e7e6
Illia Campos <camposylia@gmail.com>
2026-02-13 15:00:40 +0200

    Exercise 2.73 c

--- sicp/chapter2/73.rkt
+++ sicp/chapter2/73.rkt
@@ -81,8 +81,48 @@
   (put 'deriv '+ deriv-sum)
   (put 'deriv '* deriv-product))
 
-
 (install-deriv-package)
 
 (check-equal? (deriv '(+ (* a x) b) 'x) 'a)
 (check-equal? (deriv '(* k (+ x a)) 'x) 'k)
+
+
+(define (install-deriv-expt-package)
+  (define (=number? exp num) (and (number? exp) (= exp num)))
+  (define (make-sum a1 a2)
+    (cond [(=number? a1 0) a2]
+          [(=number? a2 0) a1]
+          [(and (number? a1) (number? a2))
+           (+ a1 a2)]
+          [else (list '+ a1 a2)]))
+  (define (make-product m1 m2)
+    (cond [(or (=number? m1 0) (=number? m2 0)) 0]
+          [(=number? m1 1) m2]
+          [(=number? m2 1) m1]
+          [(and (number? m1) (number? m2)) (* m1 m2)]
+          [else (list '* m1 m2)]))
+
+  (define (make-exponentiation base exponent)
+    (cond [(= exponent 0) 1]
+          [(number? base) (expt base exponent)]
+          [(= exponent 1) base]
+          [else (list '** base exponent)]))
+
+  (define (deriv-exponentiation expr var)
+    (define (base x) (car x))
+    (define (exponent x) (cadr x))
+    (make-product
+          (make-product (exponent expr)
+                        (make-exponentiation (base expr)
+                                             (- (exponent expr) 1)))
+          (deriv (base expr) var)))
+
+    (put 'deriv '** deriv-exponentiation))
+
+
+(install-deriv-expt-package)
+
+(check-equal? (deriv '(** (+ (* 2 x) 1) 3) 'x)
+              '(* (* 3 (** (+ (* 2 x) 1) 2)) 2))
+(check-equal? (deriv '(** (* a t) 4) 't)
+              '(* (* 4 (** (* a t) 3)) a))
